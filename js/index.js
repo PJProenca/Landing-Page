84ec6dfa0a551f0345f50cbf87791fc9ff259217
PJProenca <paulojorgeproenca@gmail.com>
2024-03-04 19:14:16 +0000

    created dropdown menu with animation - ongoing

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 function showMenu() {
 	var ele = document.getElementById("menu-bar");
-	if (ele.className ==='is-visible'){
-		ele.classList.remove("is-visible");
-	}else{
-		ele.classList.add("is-visible");
+	var btn = document.getElementById("menu-btn ");
+	if (btn.className !=='is-active'){
+		ele.style.top ='70px';
+		ele.style.opacity = '1';
+		btn.classList.add("is-active");
+	}
+	if (btn.className ==='is-active'){
+		ele.style.top ='-200px';
+		ele.style.opacity = '0';
+		btn.classList.remove("is-active");
 	}
 }
