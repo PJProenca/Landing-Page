fd58c730d0ace523e22f64d0be9e2de320c57de5
PJProenca <paulojorgeproenca@gmail.com>
2024-03-05 11:12:28 +0000

    Corrected logic for menu-bar and add image to header BG

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
-function showMenu() {
-	var ele = document.getElementById("menu-bar");
-	var open = document.getElementById("menu-btn");
-	var close = document.getElementById("close-btn");
-		open.style.display = "none";	
-		close.style.display = "block";
-		ele.style.top ='70px';
-		ele.style.opacity = '1';
-}
+const menuBtn = document.querySelector(".menu-btn");
+const menuIcon = document.querySelector(".menu-btn i");
+const menu = document.querySelector(".menu-bar");
+const menuItem = document.querySelector(".menu-bar li");
 
- function hideMenu(){
-	var ele = document.getElementById("menu-bar");
-	var open = document.getElementById("menu-btn");
-	var close = document.getElementById("close-btn");
-		open.style.display = "block";	
-		close.style.display = "none";
-		ele.style.top ='-200px';
-		ele.style.opacity = '0';
-}
+menuBtn.onclick = function () {
+	menu.classList.toggle("open");
+	const isOpen = menu.classList.contains("open");
+	menuIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+menuItem.onclick = function () {
+	menu.classList.remove("open");
+	menuIcon.classList = "fa-solid fa-bars";
+};
