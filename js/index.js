cc245fca71c7ddf7c797036d8a5098f5724d985c
PJProenca <paulojorgeproenca@gmail.com>
2024-03-05 12:22:14 +0000

    Added style to the nav bar menu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+/* Selectors */
 const menuBtn = document.querySelector(".menu-btn");
 const menuIcon = document.querySelector(".menu-btn i");
 const menu = document.querySelector(".menu-bar");
 const menuItem = document.querySelector(".menu-bar li");
+const navMenuItem = document.querySelectorAll("nav ul li");
+var itemPreviousSelected = null;
 
+/* onClick functions */
 menuBtn.onclick = function () {
 	menu.classList.toggle("open");
 	const isOpen = menu.classList.contains("open");
@@ -13,3 +17,13 @@ menuItem.onclick = function () {
 	menu.classList.remove("open");
 	menuIcon.classList = "fa-solid fa-bars";
 };
+
+navMenuItem.forEach((item) => {
+	item.onclick = function () {
+		if (itemPreviousSelected) {
+			itemPreviousSelected.classList.remove("active");
+		}
+		item.classList.add("active");
+		itemPreviousSelected = item;
+	};
+});
